80cf0ef06d792171f2f578c1a61acdf93c9796ec
hoangcony004 <nguyenduchoang522@gmail.com>
2024-08-13 20:50:54 +0700

    Khung dự án

---
+++ src/test/java/hoang_vuong/project/doan/DoanApplicationTests.java
@@ -0,0 +1,13 @@
+package hoang_vuong.project.doan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
